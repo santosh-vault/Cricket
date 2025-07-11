e3199e78d35227cb80619dd5f4c03fa9845a4032
Santosh Subedi <santossubedi33@gmail.com>
2025-07-11 13:10:04 +0545

    analytics

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
 import { Footer } from './Footer';
+import { Helmet } from 'react-helmet-async';
 
 export const Layout: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2VVQGZW2CX"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2VVQGZW2CX');
+          `}
+        </script>
+      </Helmet>
+      <div className="min-h-screen bg-gray-50">
+        <Header />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 };
